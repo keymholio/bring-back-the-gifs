84d77ad4f4d74852c5ac2e0de4a5bf80c4a546d1
Andrew Keym <andrew.keym@gmail.com>
2015-11-11 13:10:08 -0500

    adding post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var qs = require('querystring');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +10,33 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.post('/', function(request, response) {
+  var res = {
+    "color": "green",
+    "message": "It's going to be sunny tomorrow! (yey)",
+    "notify": true,
+    "message_format": "text"
+  };
+
+  var body = '';
+
+  request.on('data', function (data) {
+    body += data;
+    // Too much POST data, kill the connection!
+    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+    if (body.length > 1e6)
+      request.connection.destroy();
+  });
+
+  request.on('end', function () {
+    var post = qs.parse(body);
+    console.log(post);
+  });
+  
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify(res, null, 3));
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
